c97abb6d3b4a3080cfd1faba66bf53b6157a4c3e
zen31840 <zen31840@gmail.com>
2020-03-23 01:54:23 +0700

    edit

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,17 +1,17 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-// import gql from "graphql-tag";
+import gql from "graphql-tag";
 import { ApolloProvider } from '@apollo/react-hooks';
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import Pages from './pages';
 import injectStyles from './styles';
 import { resolvers, typeDefs } from './resolvers';
-// import Login from './pages/login';
+import Login from './pages/login';
 
 import styled from 'react-emotion';
-import { useApolloClient } from '@apollo/react-hooks';
+import { useApolloClient,useQuery } from '@apollo/react-hooks';
 
 import { menuItemClassName }  from './components/menu-item';
 import { ReactComponent as ExitIcon } from './assets/icons/exit.svg';
@@ -46,16 +46,16 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     },
   });
 
-/*   const IS_LOGGED_IN = gql`
+  const IS_LOGGED_IN = gql`
   query IsUserLoggedIn {
     isLoggedIn @client
   }
-`; */
+`;
 
-/* function IsLoggedIn() {
+function IsLoggedIn() {
   const { data } = useQuery(IS_LOGGED_IN);
   return data.isLoggedIn ? <Pages /> : <Login />;
-} */
+}
 
 /* client
   .query({
@@ -96,7 +96,7 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>, 
   document.getElementById('root')
 );
